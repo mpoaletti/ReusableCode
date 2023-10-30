45c8e5f1bb15142e6362f04b7c9a91aff580950e
efurzlan <efurzlan@uwsuper.edu>
2023-10-30 09:20:51 -0500

    Add Okta configuration code.  Web.config holds values for connecting to Okta - currently default values that need updating per application.  Startup.cs and OktaStartupLogin.cs have nuget package information and setup code.

---
+++ BoilerPlateDraft/Code_Repo/OktaStartupLogin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//Nuget packeges to install:
+//IdentityModel
+//Microsoft.AspNet.Owin
+//Microsoft.Owin
+//Okta.AspNet
+//Owin
+//Microsoft.AspNet.Identity.Core
+//Microsoft.Owin.Security.Cookies
+//Microsoft.Owin.Security.OpenIdConnect
+//IdentityModel.Client   (Version 3.80)
+//Microsoft.AspNet.Identity.Owin
+//Microsoft.Owin.Host.SystemWeb
+//Microsoft.Owin.Security.Jwt
+//Okta.AspNet
+//Microsoft.Web.Infrastructure
+
+//Create OWIN Startup file (Add New item > OWIN Startup Class) see startup.cs
+
+//usings to include in startup.cs:
+//using IdentityModel.Client;
+//using Microsoft.AspNet.Identity;
+//using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+//using Microsoft.IdentityModel.Tokens;
+//using Microsoft.Owin;
+//using Microsoft.Owin.Security;
+//using Microsoft.Owin.Security.Cookies;
+//using Microsoft.Owin.Security.OpenIdConnect;
+//using Owin;
+//using System.Collections.Generic;
+//using System.Security.Claims;
+
+
+
+namespace BoilerPlateDraft.Code_Repo
+{
+	public class OktaStartupLogin
+    {
+
+		//Login check and pull username from OKTA login credentials:
+		//if (!Request.IsAuthenticated) {
+                //Session["logInStatus"] = false;
+                //Response.Redirect("WEBSITEPAGE", false);
+            //}
+        //else {
+            //Session["Username"] = HttpContext.Current.GetOwinContext().Request.User.Identity.Name;
+            //Session["logInStatus"] = true;
+        //}
+	}
+}
